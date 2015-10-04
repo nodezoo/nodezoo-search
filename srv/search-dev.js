87d8d4d63c7f8266751b79716b279de9255f787c
Richard Rodger <richard@ricebridge.com>
2015-10-04 16:57:56 +0100

    remove stub

--- srv/search-dev.js
+++ srv/search-dev.js
@@ -4,10 +4,3 @@ require('seneca')()
   .use('../search.js')
   .listen(44002)
   .repl(43002)
-
-  .add('role:search,cmd:search',function(msg,done){
-    done(null,{items:[{
-      name:'bar',
-      version:'1.0.0'
-    }]})
-  })
