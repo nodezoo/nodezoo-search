8fb917beaba1f7b2de500d06327c317d8b107a23
Richard Rodger <richard@ricebridge.com>
2015-12-16 12:02:01 +0000

    package.json

--- srv/search-dev.js
+++ srv/search-dev.js
@@ -2,5 +2,6 @@ var ELASTIC = process.env.ELASTIC || 'localhost'
 
 require('seneca')()//{debug:{act_caller:true}})
   .use('../search.js',{elastic:{host:ELASTIC}})
+
   .use('mesh',{auto:true, pin:'role:search'})
-  .repl(33002)
+
