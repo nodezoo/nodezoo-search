bae76929ec7506ac161d26ccbf7f7d7089737248
Richard Rodger <richard@ricebridge.com>
2015-12-17 12:00:56 +0000

    msgstats

--- srv/search-dev.js
+++ srv/search-dev.js
@@ -1,7 +1,12 @@
 var ELASTIC = process.env.ELASTIC || 'localhost'
+var STATS = process.env.STATS || 'localhost'
 
-require('seneca')()//{debug:{act_caller:true}})
+require('seneca')()
   .use('../search.js',{elastic:{host:ELASTIC}})
+
+  .use('msgstats',{
+    udp: { host: STATS }
+  })
 
   .use('mesh',{auto:true, pin:'role:search'})
 
