42a7535bb62aa19f2e7da0ed8fe55fe6b09a0c15
Kristina Matuleviciute <kristina.matuleviciute@nearform.com>
2016-01-19 17:24:32 +0000

    linting started in search file

--- search.js
+++ search.js
@@ -1,41 +1,42 @@
 /* Copyright (c) 2014-2015 Richard Rodger, MIT License */
 /* jshint node:true, asi:true, eqnull:true */
-"use strict";
+'use strict';
 
 
-var request = require('request')
+const request = require('request');
 
 
 module.exports = function search( options ){
-  var seneca = this
+    const seneca = this;
 
-  options = seneca.util.deepextend({
-    elastic: {
-      host: 'localhost',
-      port: 9200,
-      base: 'zoo'
-    },
-  },options)
-
-
-  seneca.add( 'role:search,cmd:insert', cmd_insert )
-  seneca.add( 'role:search,cmd:search', cmd_search )
+    options = seneca.util.deepextend({
+        elastic: {
+            host: 'localhost',
+            port: 9200,
+            base: 'zoo'
+        }
+    },options);
 
 
-  function cmd_insert( args, done ) {
-    var seneca  = this
+    seneca.add( 'role:search,cmd:insert', cmd_insert );
+    seneca.add( 'role:search,cmd:search', cmd_search );
 
-    var elastic = options.elastic
 
-    var url = 'http://'+elastic.host+':'+elastic.port+'/'+elastic.base+
-          '/mod/'+args.data.name
 
-    request(
-      {
-        url:    url,
-        method: 'POST',
-        json:   args.data
-      }, 
+
+    const cmd_insert = function ( args, done ) {
+        const seneca  = this;
+
+        const elastic = options.elastic;
+
+        const url = 'http://' + elastic.host + ' : ' + elastic.port + '/' + elastic.base + '/mod/' + args.data.name;
+
+        request(
+            {
+                url:    url,
+                method: 'POST',
+                json:   args.data
+            },
       function(err,res,body){
         done(err,body)
       })
@@ -54,7 +55,7 @@ module.exports = function search( options ){
       {
         url:    url,
         method: 'GET',
-      }, 
+      },
       function(err,res,body){
         if( err ) return done(err);
 
@@ -74,6 +75,5 @@ module.exports = function search( options ){
       })
   }
 
-    
-}
 
+}
