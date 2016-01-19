cef6ec5999909c1092268be1325d775fa5bcec53
Dean McDonnell <mcdonnelldean@outlook.com>
2016-01-19 12:39:06 +0000

    Removed msgstats here till I can debug issue

--- srv/search-dev.js
+++ srv/search-dev.js
@@ -3,10 +3,4 @@ var STATS = process.env.STATS || 'localhost'
 
 require('seneca')()
   .use('../search.js',{elastic:{host:ELASTIC}})
-
-  .use('msgstats',{
-    udp: { host: STATS }
-  })
-
   .use('mesh',{auto:true, pin:'role:search'})
-
