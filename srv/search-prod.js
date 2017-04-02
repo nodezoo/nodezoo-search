315ed4c3deb95ac237e5cc6092ad881f5c9c4b44
Richard Rodger <richard@ricebridge.com>
2017-04-02 18:37:37 +0100

    prod

---
+++ srv/search-prod.js
@@ -0,0 +1,30 @@
+/* Copyright (c) 2014-2017 Richard Rodger and other contributors, MIT License */
+
+var BASES = process.env.BASES.split(',')
+var ELASTIC = process.env.ELASTIC_SERVICE_HOST || 'localhost'
+
+var Seneca = require('seneca')
+
+Seneca({tag: 'search'})
+  .test('print')
+
+  .use('../search.js', {
+    elastic: {
+      host: ELASTIC
+    }
+  })
+
+  .use('mesh', {
+    pin: 'role:search',
+    bases: BASES,
+    host: '@eth0',
+    sneeze: {silent:false}
+  })
+
+  .ready(function () {
+    this.add('role:search,cmd:search', function (msg, reply) {
+      this.prior(msg, reply)
+
+      this.act('role:suggest,cmd:add,query:'+msg.query)
+    })
+  })
